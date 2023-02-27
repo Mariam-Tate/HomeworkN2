212b8f5ae6b873b0609b57f14605b68a6dc4cc6d
Mariam Tateladze <ma.tateladze@gmail.com>
2023-02-27 11:09:10 +0400

    fist commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            var name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(name);
+            Console.ResetColor();
+        }
+    }
+}
